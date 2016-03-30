ebe567a1fcac1d76bf2014ce8b70816f69ab1274
Héctor Álvarez Ibáñez <hec_insti@hotmail.com>
2016-03-30 18:28:46 +0200

    Añadidos las clases del modelo

---
+++ src/main/java/es/uniovi/asw/model/VotoConfirmado.java
@@ -0,0 +1,52 @@
+package es.uniovi.asw.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.ManyToOne;
+
+import es.uniovi.asw.model.types.VotoConfirmadoKey;
+
+@Entity
+@IdClass( VotoConfirmadoKey.class )
+public class VotoConfirmado {
+	
+	@Id @GeneratedValue
+	private Long id;
+	
+	@Id @ManyToOne private Voter votante;
+	@Id @ManyToOne private Eleccion eleccion;
+	private boolean haVotado;
+	
+	VotoConfirmado() {}
+	
+	public VotoConfirmado(Voter voter, Eleccion eleccion){
+		Asociacion.EleccionVotoConfirmado.link(eleccion, this);
+		Asociacion.VotanteVotoConfirmado.link(voter, this);
+	}
+	
+	public Voter getVotante() {
+		return votante;
+	}
+	public void setVotante(Voter votante) {
+		this.votante = votante;
+	}
+	public Eleccion getEleccion() {
+		return eleccion;
+	}
+	public void setEleccion(Eleccion eleccion) {
+		this.eleccion = eleccion;
+	}
+	public boolean isHaVotado() {
+		return haVotado;
+	}
+	public void setHaVotado(boolean haVotado) {
+		this.haVotado = haVotado;
+	}
+	
+	@Override
+	public String toString() {
+		return "VotoConfirmado [votante=" + votante + ", eleccion=" + eleccion + ", haVotado=" + haVotado + "]";
+	}
+}
