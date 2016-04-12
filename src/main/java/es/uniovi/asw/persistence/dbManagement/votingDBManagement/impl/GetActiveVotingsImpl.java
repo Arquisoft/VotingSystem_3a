f289ae460d22c9dca83bfe2f0f32af9bf8458ac8
Héctor Álvarez Ibáñez <hec_insti@hotmail.com>
2016-04-12 19:46:33 +0200

    Un par de imports que se me olvido añadir

--- src/main/java/es/uniovi/asw/persistence/dbManagement/votingDBManagement/impl/GetActiveVotingsImpl.java
+++ src/main/java/es/uniovi/asw/persistence/dbManagement/votingDBManagement/impl/GetActiveVotingsImpl.java
@@ -1,7 +1,5 @@
 package es.uniovi.asw.persistence.dbManagement.votingDBManagement.impl;
 
-import es.uniovi.asw.model.Voter;
-import es.uniovi.asw.persistence.dbManagement.repository.VoterRepository;
 import es.uniovi.asw.persistence.dbManagement.votingDBManagement.GetActiveVotings;
 
 class GetActiveVotingsImpl implements GetActiveVotings {
