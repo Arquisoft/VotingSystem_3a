125720d93ddfe46e19a0aff60a124e1f52bedf2f
Darío Rodríguez García <dario.rg.inf@gmail.com>
2016-04-13 18:51:27 +0200

    Modificados test

--- src/test/java/es/uniovi/asw/CucumberTest.java
+++ src/test/java/es/uniovi/asw/CucumberTest.java
@@ -1,13 +1,11 @@
 package es.uniovi.asw;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@IntegrationTest({ "server.port=0" })
 @CucumberOptions(features = "src/test/resources/features")
 public class CucumberTest{
 }
