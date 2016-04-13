f3929ebdeca58d569af9c9eef3b0d568b9481fe1
Darío Rodríguez García <dario.rg.inf@gmail.com>
2016-04-13 18:36:39 +0200

    Añadidas 2 historias de usuario probadas con cucumber

--- src/test/java/es/uniovi/asw/CucumberTest.java
+++ src/test/java/es/uniovi/asw/CucumberTest.java
@@ -1,16 +1,13 @@
 package es.uniovi.asw;
 
-//import org.junit.runner.RunWith;
-//
-//import cucumber.api.CucumberOptions;
-//import cucumber.api.junit.Cucumber;
-//
-//@RunWith(Cucumber.class)
-//// @RunWith(SpringJUnit4ClassRunner.class)
-////@SpringApplicationConfiguration(classes = Application.class)
-//// @ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
-////@WebAppConfiguration
-////@IntegrationTest({ "server.port=0" })
-//@CucumberOptions(features = "src/test/resources/features")
-//public class CucumberTest{
-//}
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@IntegrationTest({ "server.port=0" })
+@CucumberOptions(features = "src/test/resources/features")
+public class CucumberTest{
+}
