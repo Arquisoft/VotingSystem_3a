a39108af1b2fc63c90698286b7473c5c06955bfd
jorgevilasuarez <pequejorge@hotmail.com>
2016-04-15 18:22:37 +0200

    Implementadas las clases CheckVote y CheckVoteImpl

--- src/main/java/es/uniovi/asw/business/registerVote/storePhisicalVoteManagement/impl/CheckVoteImpl.java
+++ src/main/java/es/uniovi/asw/business/registerVote/storePhisicalVoteManagement/impl/CheckVoteImpl.java
@@ -1,5 +1,21 @@
 package es.uniovi.asw.business.registerVote.storePhisicalVoteManagement.impl;
 
-public class CheckVoteImpl {
+import java.util.List;
 
+import es.uniovi.asw.business.registerVote.storePhisicalVoteManagement.CheckVote;
+import es.uniovi.asw.model.VotoConfirmado;
+import es.uniovi.asw.persistence.dbManagement.repository.ConfirmedVoteRepository;
+
+public class CheckVoteImpl implements CheckVote{
+
+	@Override
+	public boolean alreadyVoted(ConfirmedVoteRepository cvRep, Long idVotante, Long idEleccion) {
+		List<VotoConfirmado> confirmados = cvRep.findAll();
+		for(VotoConfirmado vc : confirmados){
+			if(vc.getEleccion().getId().equals(idEleccion) && vc.getVotante().getId().equals(idVotante)){
+				return vc.isHaVotado();
+			}
+		}
+		return false;
+	}
 }
