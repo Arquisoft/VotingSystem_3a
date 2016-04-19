6272fdb867edd764495ff69e8487639ec116805e
Héctor Álvarez Ibáñez <hec_insti@hotmail.com>
2016-04-19 19:01:00 +0200

    Añadidos metodos de prueba del controlador. Parte de votingSystem

--- src/test/java/es/uniovi/asw/MainControllerTest.java
+++ src/test/java/es/uniovi/asw/MainControllerTest.java
@@ -6,17 +6,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import es.uniovi.asw.model.Candidatura;
+import es.uniovi.asw.model.Voter;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -62,5 +68,15 @@ public class MainControllerTest {
   @Test
   public void adminIndexConfPollings() throws Exception {
 	  mvc.perform(post("/admin_index").param("pollingStation", "")).andExpect(status().isOk()).andExpect(content().string(containsString("de configuración de colegios")));
+  }
+  
+  @Test
+  public void voterIndex() throws Exception {
+	  mvc.perform(post("/voter_index").session(new MockHttpSession()).sessionAttr("voter", new Voter("Prueba", "a@b.c","abc","0000000002")).param("vote" , "1")).andExpect(status().isOk()).andExpect(content().string(containsString("Opciones")));
+  }
+  
+  @Test
+  public void voterVote() throws Exception {
+	  mvc.perform(post("/show_options").session(new MockHttpSession()).sessionAttr("voter", new Voter("Prueba", "a@b.c","abc","0000000002")).sessionAttr("opciones", new ArrayList<Candidatura>()).param("decision" , "1")).andExpect(status().isOk()).andExpect(content().string(containsString("Sistema de votación")));
   }
 }
